13c8950f94eec753d360ce127ded517d9e73332c
Wes Payne <wes@noblepayne.com>
2016-12-23 16:08:51 -0800

    5-2 complete. Not very efficient however.

--- src/advent_of_code/day5/1.clj
+++ src/advent_of_code/day5/1.clj
@@ -28,30 +28,35 @@
        (map #(nth % 5))
        (apply str)))
 
-(defn valid-output-position? [in-hash]
+;; should be 2.clj here
+
+(defn valid-output-position?
+  "hacky soln that checks if int ascii value is between \0 and \7"
+  [in-hash]
   (let [pos (int (nth in-hash 5))]
     (and (>= pos 48)
          (> 56 pos))))
 
+(defn password-reducer
+  "take seq of vector pairs of [position value] and produce
+  a seq of length 8 with one value per position"
+  [coll elem]
+  (if (= 8 (count coll))
+    (reduced coll)
+    (if (some #{(first elem)} (map (partial first) coll))
+      coll
+      (conj coll elem))))
 
-(defn password-from-hashes [hashlist]
-  (->> hashlist
-       (map (fn [x] [(nth x 5) (nth x 6)]))
+(defn real-decrypt-password
+  "decrypt password. try to figure out a way to make this parallel..."
+  [password]
+  (->> password
+       (hash-seq)
+       (filter starts-with-five-zeros?)
+       (filter valid-output-position?)
+       (map #(vector (nth % 5) (nth % 6)))
+       (reduce password-reducer [])
+       (map #(vector (- (int (first %)) 48) (second %)))
        (sort)
        (map #(second %))
        (apply str)))
-
-(defn real-decrypt-password [password]
-  (->> password
-       (hash-seq)
-       (filter starts-with-five-zeros?)
-       (filter valid-output-position?)
-       (take 8)
-       (password-from-hashes)))
-
-(defn real-decrypt-password-test [password]
-  (->> password
-       (hash-seq)
-       (filter starts-with-five-zeros?)
-       (filter valid-output-position?)
-       (take 8)))
